9bbbbab33ce7b77d75aff44e4d8db60ea046a7e9
Chefkwami <sitsofefeyi28@gmail.com>
2025-10-23 21:41:56 +0100

    fixes in otp

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,44 +3,43 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import transporter from "../config/nodeMailer.js";
 
-
-
-
-
-
 export const register = async(req, res) => {
-
     const { firstname, lastname, email, password } = req.body;
 
     if (!firstname || !lastname || !email || !password) {
         return res.status(400).json({
             success: false,
-            message: "Missing fields"
-        })
+            message: "Missing fields",
+        });
     }
 
     try {
-
-        const existingUser = await User.findOne({ email })
+        const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(409).json({
                 success: false,
-                message: "User already exist"
-            })
+                message: "User already exist",
+            });
         }
 
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const theUser = User({
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword,
+        });
+        await theUser.save();
 
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = User({ firstname, lastname, email, password: hashedPassword })
-        await newUser.save();
-
-        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        const token = jwt.sign({ id: theUser._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        })
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
 
         //sending welcome email
 
@@ -48,63 +47,57 @@ export const register = async(req, res) => {
             from: process.env.SENDER_EMAIL,
             to: email,
             subject: "Welcome to Lyte",
-            text: `Your account has been successfuly created`
+            text: `Your account has been successfuly created`,
+        };
 
-        }
-
-        await transporter.sendMail(mailOptions)
-
+        await transporter.sendMail(mailOptions);
 
         return res.status(201).json({
             success: true,
-            message: "Account successfully created"
-        })
-
-
+            message: "Account successfully created",
+        });
     } catch (error) {
         return res.status(500).json({
             success: false,
-            message: ('Server error', error)
-        })
+            message: error.message,
+        });
     }
-}
-
-
+};
 
 export const login = async(req, res) => {
-
     const { email, password } = req.body;
 
     try {
         if (!email || !password) {
             return res.status(400).json({
                 success: false,
-                message: "Missing fields, kindly input your details correctly"
-            })
+                message: "Missing fields, kindly input your details correctly",
+            });
         }
 
-
-        const existingUser = await User.findOne({ email })
-        if (!existingUser) {
+        const theUser = await User.findOne({ email });
+        if (!theUser) {
             return res.status(404).json({
                 success: false,
-                message: "Invalid email"
-            })
+                message: "Invalid email",
+            });
         }
 
-        const isMatch = await bcrypt.compare(password, existingUser.password)
+        const isMatch = await bcrypt.compare(password, theUser.password);
         if (!isMatch) {
             return res.status(404).json({
                 success: false,
-                message: "Invalid password"
-            })
+                message: "Invalid password",
+            });
         }
-        const token = jwt.sign({ id: User._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        const token = jwt.sign({ id: theUser._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
         //sending login email
@@ -112,30 +105,23 @@ export const login = async(req, res) => {
         const mailOptions = {
             from: process.env.SENDER_EMAIL,
             to: email,
-            subject: "Account Login",
-            text: `Account login successful, if you were not the one. Kindly call customer care.`
+            subject: "Login Alert",
+            text: `Account login successful, if you were not the one. Kindly call customer care.`,
+        };
 
-        }
-
-        await transporter.sendMail(mailOptions)
+        await transporter.sendMail(mailOptions);
 
         return res.status(200).json({
             success: true,
-            message: "Login successful"
-        })
-
+            message: "Login successful",
+        });
     } catch (error) {
-        console.log("Server error", error)
         return res.status(500).json({
             success: false,
-            message: "Server error"
-
-        })
+            message: error.message,
+        });
     }
-
-
-}
-
+};
 
 export const getAllUsers = async(req, res) => {
     try {
@@ -144,67 +130,138 @@ export const getAllUsers = async(req, res) => {
             return res.status(200).json({
                 success: true,
                 message: "Users successfuly fetched",
-                data: allUsers
+                data: allUsers,
             });
         } else {
             return res.status(404).json({
                 success: false,
-                message: "No user in the database"
+                message: "No user in the database",
             });
         }
     } catch (error) {
-        console.log("Server error", error);
-        res.status(500).json({
+        return res.status(500).json({
             success: false,
-            message: 'Server Error'
-        })
+            message: error.message
+        });
     }
-}
-
+};
 
 export const deleteUser = async(req, res) => {
-
-
     try {
-        const userId = req.params.id;
+        const theUserId = req.params.id;
 
-        const deletedUser = await User.findByIdAndDelete(userId);
+        const deletedUser = await User.findByIdAndDelete(theUserId);
         if (!deletedUser) {
             return res.status(404).json({
                 success: false,
-                message: "User not found"
-
+                message: "User not found",
             });
-
         } else {
             return res.status(200).json({
                 success: true,
                 message: "User successfully deleted",
-                data: deletedUser
+                data: deletedUser,
             });
         }
-
-
-
     } catch (error) {
-        console.log("Server Error", error);
+
         return res.status(500).json({
             success: false,
-            message: "Server Error"
-
+            message: error.message
         });
     }
-}
-
+};
 
 export const logout = async(req, res) => {
-    res.cookie('token', {
+    res.cookie("token", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     });
     return res.status(200).json({
         success: true,
-        message: "Logged Out"
-    })
-}
+        message: "Logged Out",
+    });
+};
+
+export const sendVeriftyOtp = async(req, res) => {
+    try {
+        const { userId } = req.body;
+        const theUser = await User.findById(userId);
+        if (theUser.isAccountVerified === "true") {
+            return res.status(403).json({
+                success: false,
+                message: "Account already verified",
+            });
+        }
+
+        const otp = String(Math.floor(Math.random() * 900000 + 100000));
+        theUser.verifyOtp = otp;
+
+        theUser.verifyOtpExpireAt = Date.now() + 7 * 24 * 60 * 60 * 100;
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: theUser.email,
+            subject: "OTP VERIFICATION",
+            text: `Here is your ${otp} to verify your account`,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        await theUser.save();
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+export const verifyEmail = async(req, res) => {
+    const { userId, otp } = req.body;
+    try {
+        if (!userId || !otp) {
+            return res.status(401).json({
+                success: false,
+                message: "Missing fields",
+            });
+        }
+
+        const theUser = await User.findById(userId);
+        if (!theUser) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        if (theUser.verifyOtp === "" || theUser.verifyOtp !== otp) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid OTP",
+            });
+        }
+
+        if (theUser.verifyOtpExpireAt < Date.now()) {
+            return res.status(400).json({
+                success: false,
+                message: "OTP expired",
+            });
+        }
+
+        theUser.isAccountVerified = true;
+        theUser.verifyOtp = "";
+        theUser.verifyOtpExpireAt = 0;
+        await theUser.save();
+        return res.status(200).json({
+            success: true,
+            message: "Email verified successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { deleteUser, getAllUsers, login, register } from "../controllers/authController.js";
+import { deleteUser, getAllUsers, login, register, sendVeriftyOtp, verifyEmail } from "../controllers/authController.js";
+import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router()
 
@@ -11,6 +12,10 @@ authRouter.post("/register", register)
 authRouter.post("/login", login)
 
 authRouter.get("/users", getAllUsers)
+
+authRouter.post("/verify-otp", userAuth, sendVeriftyOtp)
+
+authRouter.post("/verify-account", userAuth, verifyEmail)
 
 authRouter.post("/:id", deleteUser)
 
